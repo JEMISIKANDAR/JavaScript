375dfb2cb2ca9611571b6e87e67745fd89f27305
JEMISIKANDAR <100154702+JEMISIKANDAR@users.noreply.github.com>
2024-01-26 13:45:01 +0000

    Tut 2

---
+++ 01_Basics_JS/tut2.js
@@ -0,0 +1,8 @@
+const accountID = 25;
+let accountName = "Let me Hurt You";
+let accountType = "Current";
+let accountPassword = "212121";
+let accountState;
+
+console.log([accountID, accountName, accountType, accountPassword, accountState]);
+console.table([accountID, accountName, accountType, accountPassword, accountState]);
