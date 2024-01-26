9439a25d82bf1717675fbb4b326f147c5f66a3be
JEMISIKANDAR <100154702+JEMISIKANDAR@users.noreply.github.com>
2024-01-26 14:22:24 +0000

    Datatypes

---
+++ 01_Basics_JS/tut3Datatypes.js
@@ -0,0 +1,7 @@
+let name = "JEMI"    // String
+let age = 20;        // integer
+let marrige = false;    // Boolean
+let status = null;           // Null
+let type5 = undefined;          // Undefined
+
+console.table([name, age, marrige, status, type5]);
