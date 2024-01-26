dcde4bc05398c4cebff99b616e2b79ed7e2443b7
JEMISIKANDAR <100154702+JEMISIKANDAR@users.noreply.github.com>
2024-01-26 15:23:51 +0000

    datatypeConversion

---
+++ 01_Basics_JS/tut4Datatype_Conversion.js
@@ -0,0 +1,16 @@
+let score = 34;
+let score2 = "34";
+
+console.log(typeof score);
+console.log(typeof(score2));
+
+let intString = String(score);
+console.log(typeof intString);
+console.log(score);
+
+let marrige = 1;
+
+let toBoolean = Boolean(marrige);
+
+console.log(typeof toBoolean);
+console.log(toBoolean);
