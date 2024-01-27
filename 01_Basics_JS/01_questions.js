4fb255b90ff826093f559414698903a2d2700b2b
JEMISIKANDAR <100154702+JEMISIKANDAR@users.noreply.github.com>
2024-01-27 16:48:08 +0000

    Num and Maths

---
+++ 01_Basics_JS/01_questions.js
@@ -0,0 +1,20 @@
+const product = {
+    title: "NoteBook",
+    rating: 4.5,
+    offer:  5,
+    price:  250,
+    is_deal: true,
+};
+
+const insta_profile = {
+    Username: "jameel__sikandar",
+    Fullname: "Jameel Sikandar",
+    Posts: 6,
+    Followers: "175k",
+    Following: 274,
+    Bio: "And this life of the world is only an ammusiment and a play"
+
+};
+
+console.table(insta_profile);
+console.log(typeof insta_profile);
---
+++ 01_Basics_JS/tut5_NumAndMath.js
@@ -0,0 +1,12 @@
+const number = 500;
+
+const score = new Number(500);
+
+console.log(number);
+
+console.log(score);
+
+console.log(typeof number);
+
+console.log(score.toString().length);
+console.log(typeof score);
