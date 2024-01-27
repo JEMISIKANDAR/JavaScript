a4e5ad48c0138e38dab8b945fdb5f767308c461e
JEMISIKANDAR <100154702+JEMISIKANDAR@users.noreply.github.com>
2024-01-27 19:05:43 +0000

    Arrays tut1

---
+++ 02_Arrays/tut1_Array.js
@@ -0,0 +1,20 @@
+let myArray = [1, 20, "JEMI", 13.5, "Hello World"];
+
+myArray[2] = "Jameel Sikandar";
+
+// myArray.unshift("Hello");         // add new elemnt to beginning of array
+
+// myArray.push("Animal");     // add new element
+
+// myArray.pop();                // pop() removes last element
+
+// myArray.shift();              // removes forst element
+
+// console.table(myArray);
+
+// console.log(myArray.length);
+
+ // printing using loop
+for(let i = 0; i < myArray.length; i++){
+    console.log(myArray[i]);
+}
