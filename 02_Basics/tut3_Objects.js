499b27d0579031ab4b401b37305f3e658a7f268a
JEMISIKANDAR <100154702+JEMISIKANDAR@users.noreply.github.com>
2024-01-27 19:54:54 +0000

    object tut3

---
+++ 02_Basics/tut3_Objects.js
@@ -0,0 +1,11 @@
+const my_profile = {
+    fullname: "Jameel Sikandar",
+    age: 19,
+    username: "jameel__sikandar",
+    posts: 7,
+    followers: 225,
+    following: 334,
+    isFollow: true,
+}
+
+console.log(my_profile.username);
