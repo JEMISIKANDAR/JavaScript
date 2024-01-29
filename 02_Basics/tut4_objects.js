623f43a3afe6e9ab950b581ed1cb2e3a7c935a24
JEMISIKANDAR <100154702+JEMISIKANDAR@users.noreply.github.com>
2024-01-29 16:39:32 +0000

    Objects tut2

---
+++ 02_Basics/tut4_objects.js
@@ -0,0 +1,62 @@
+// Singleton
+// const facebookUser = new Object();
+
+// non-singleton
+
+const facebookUser = {}
+
+facebookUser.id = "123abc";
+facebookUser.name = "JEMI";
+facebookUser.isLoggedin = false;
+
+// console.log(facebookUser);
+
+const regularUser = {
+    email: "someone@email.com",
+    fullname: {
+        username: {
+            firstname: "Jameel",
+            lastname: "Sikandar",
+        }
+    }
+}
+// console.log(regularUser.fullname.username.lastname);     // accessed via . notation
+
+// Objects combining
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4);   
+
+const obj3 = {...obj1, ...obj2, ...obj4}    // relaible meathod for combining
+
+
+// console.log(obj3);
+
+// object inside array
+
+const user = [
+    {
+        is: 1,
+        email: "so@mail.com",
+
+    },
+    {
+        is: 2,
+        email: "so@mail.com",
+
+    },
+    {
+        is: 3,
+        email: "so@mail.com",
+
+    },
+]
+
+console.log(facebookUser);
+
+console.log(Object.keys(facebookUser));  // it makes array of an object very usefull
+console.log(Object.values(facebookUser));
+
