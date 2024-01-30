0729b189decef632443198a1d9dad05c78bac57c
JEMISIKANDAR <100154702+JEMISIKANDAR@users.noreply.github.com>
2024-01-30 16:29:11 +0000

    more functions

---
+++ 02_Basics/tut6_Functions.js
@@ -0,0 +1,38 @@
+// shopping cart issue
+function calculateCartPrice(...num1){
+   return num1
+}
+
+//console.log(calculateCartPrice(200, 500, 600))
+
+const course = {
+    name: "Jameel",
+    price: 2999,
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`)
+}
+
+// handleObject(course)
+
+// handleObject({
+//     name: "Jenny",
+//     price: 199
+// })
+
+  // for Array
+
+const myArray = [200, 500, 600, 700]
+
+// creating function for above array to get its values
+
+function getvalues(getarray){
+    return getarray[0]
+}
+
+// console.log(getvalues(myArray))
+
+console.log(getvalues([200, 400, 500, 1000]))
+
+
